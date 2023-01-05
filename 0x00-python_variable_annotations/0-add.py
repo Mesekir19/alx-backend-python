a9eda3156cfec7aad4ce41ca2762eb39913b3104
Mesekir19 <mesekirsalgech@example.com>
2023-01-05 22:59:44 +0300

    commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""adds two float numbers"""
+
+
+def add(a: float, b: float) -> float:
+    """A function which retrun sum of two float numbers"""
+
+    return (a + b)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""defines a function that returns floor"""
+
+
+def floor(n: float) -> int:
+    """returns a floor of the given float number"""
+
+    return int(n)
